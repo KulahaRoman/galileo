ec74ae45091b8cd5ea6f4e77214f58cf735673af
KulahaRoman <romankulaha2010@gmail.com>
2024-12-27 21:38:50 +0100

    Added render toggling by key press, changes in marker drawing

--- client/moonloader/galileo/render/PlayerRenderer.lua
+++ client/moonloader/galileo/render/PlayerRenderer.lua
@@ -3,7 +3,7 @@ local Color = require("galileo.util.Color")
 local Vector2D = require('galileo.util.Vector2D')
 local Vector3D = require('galileo.util.Vector3D')
 
-local RENDER_MIN_DISTANCE = 50
+local RENDER_MIN_DISTANCE = 0
 
 local PlayerRenderer = {}
 PlayerRenderer.__index = PlayerRenderer
@@ -11,6 +11,7 @@ PlayerRenderer.__index = PlayerRenderer
 function PlayerRenderer.new()
     local object = setmetatable({}, PlayerRenderer)
     object.font = renderCreateFont('Verdana', 12, FontFlag.BOLD + FontFlag.SHADOW)
+    object.enabled = false
 
     return object
 end
@@ -30,8 +31,9 @@ function PlayerRenderer:renderCircle(x, y, radius, color)
 
     renderVertex(x, y)
 
-    for i = 0, 60 do
-        local angle = math.rad((i / 60) * 360)
+    local segments = 30
+    for i = 0, segments do
+        local angle = math.rad((i / segments) * 360)
         local px = x + math.cos(angle) * radius
         local py = y + math.sin(angle) * radius
         renderVertex(px, py)
@@ -47,24 +49,28 @@ end
 
 function PlayerRenderer:renderPlayerMarker(coords, color)
     local a, r, g, b = Color.explode(color)
-    local playerColor = Color.implode(180, r, g, b)
+    local playerColor = Color.implode(200, r, g, b)
     local borderColor = Color.implode(100, 0, 0, 0)
-    local marginColor = Color.implode(180, 255, 255, 255)
+    local marginColor = Color.implode(150, 255, 255, 255)
 
-    PlayerRenderer:renderCircle(coords.x, coords.y, 12, borderColor)
-    PlayerRenderer:renderCircle(coords.x, coords.y, 11, marginColor)
-    PlayerRenderer:renderCircle(coords.x, coords.y, 9, borderColor)
-    PlayerRenderer:renderCircle(coords.x, coords.y, 8, playerColor)
+    PlayerRenderer:renderCircle(coords.x, coords.y, 13, borderColor)
+    PlayerRenderer:renderCircle(coords.x, coords.y, 12, marginColor)
+    PlayerRenderer:renderCircle(coords.x, coords.y, 10.5, borderColor)
+    PlayerRenderer:renderCircle(coords.x, coords.y, 9.5, playerColor)
 end
 
 function PlayerRenderer:render(player)
+    if isKeyJustPressed(VK_P) and not sampIsCursorActive() then
+        self.enabled = not self.enabled
+    end
+
     local playerCoords = player.state.pedCoords
     local cameraCoords = Vector3D.new(getActiveCameraCoordinates())
     local cameraPointCoords = Vector3D.new(getActiveCameraPointAt())
     local cameraToPlayerDistance = Vector3D.magnitude(Vector3D.sub(playerCoords, cameraCoords))
 
     if PlayerRenderer.isPointInView(playerCoords, cameraCoords, cameraPointCoords) and 
-            cameraToPlayerDistance > RENDER_MIN_DISTANCE then
+            cameraToPlayerDistance > RENDER_MIN_DISTANCE  and self.enabled then
         local screenCoords = Vector2D.new(convert3DCoordsToScreen(playerCoords.x, playerCoords.y, playerCoords.z))
 
         PlayerRenderer:renderPlayerMarker(screenCoords, player.state.pedColor)
